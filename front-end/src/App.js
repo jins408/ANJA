bbccba7d5468039d2dc8937f5bfbd2c6b077f820
김진실 <kjin_048@naver.com>
2020-10-26 18:33:30 +0900

    header 구조 및 css 완료

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
 // import Join from './components/Join'
-import { Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
+import { Route, useLocation } from 'react-router-dom'
 
 import Introduce from './pages/Introduce'
 import Join from './components/Join'
+import Home from './pages/Home'
+import About from './pages/About'
 
 const App = () => {
+  let location = useLocation()
+  if(location.pathname === "/"){
+    return <Route exact path="/" component={Introduce}></Route>
+  }
   return (
     <>
-      <Route exact path="/" component={Introduce}></Route>
+    <Navigation />
+      <Route exact path="/home" component={Home} />
+      <Route path="/about" component={About} />
       <Route exact path="/join" component={Join}></Route>
     </>
   );
