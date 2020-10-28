429e348b47ea5ab4e7f6892749e8f759460cfb56
김진실 <kjin_048@naver.com>
2020-10-28 15:38:03 +0900

    s03P31B101-41/Update Loginpage

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,11 +1,119 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
 
 const Login = () =>{
+    const classes = useStyles();
+
     return(
-            <div>
-                <h1>로그인</h1>
+            // <div className="container">
+            //     <h2 className="LoginHeader text-center mb-3"> 로그인 </h2> 
+            //     <form className="LoginForm"> 
+            //             <input type="email" 
+            //                    className="form-control" 
+            //                    placeholder="이메일" 
+            //                    name="email" />
+            //             <input type="password" 
+            //                    className="form-control" 
+            //                    placeholder="비밀번호" 
+            //                    name="phone" /> 
+            //             <button className="btn btn-lg btn-primary btn-block" type="button"> 로그인 </button> 
+            //     </form>
+
+            // </div>
+            <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+              <Avatar className={classes.avatar}>
+                {/* <LockOutlinedIcon /> */}
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                로그인
+              </Typography>
+              <form className={classes.form} noValidate>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  로그인
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                    비밀번호를 잊으셨나요?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="/Join" variant="body2">
+                        아직 회원이 아니신가요?
+                      {/* {"Don't have an account? Sign Up"} */}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </form>
             </div>
+          </Container>
         );
     }
 
