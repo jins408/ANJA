65005c020deccedf4361868bd6c0e44a19123e4e
조민기 <hodduco_o@naver.com>
2020-11-06 17:33:07 +0900

    S03P31B101-134 /Update 지하철 신고 Firestore 작업중

--- back-end/reports/views.py
+++ back-end/reports/views.py
@@ -16,54 +16,35 @@ cred = credentials.Certificate("taste-ac33e-firebase-adminsdk-u8rj0-fc33c78348.j
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+
 class ReportView(APIView):
-    def get(self, reqeust):
-        queryset = Report.objects.all()
-        serializer = ReportSerializer(queryset, many=True)
-        return Response({'data': 'test'}, status=status.HTTP_200_OK)
+    def get(self, request):
+        doc_ref = getCollection(request.GET.get("id")).document(request.GET.get("reportDocId"))
+        doc = doc_ref.get().to_dict()
+        return Response({'data': doc}, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
         # 01 / 01 / 01 => 호선 / 열차 / 칸
         # date_time = datetime.now().strftime("%Y/%m/%d/, %H:%M:%S")
         newReport = {
+            'line': request.data["id"][:2],
+            'sid': request.data["id"][2:4],
+            'ssid': request.data["id"][4:6],
             'category': request.data["category"],
-            'sid': request.data["sid"],
             'contents': request.data["contents"],
             'time': datetime.now(),
         }
-        doc_ref = getCollection(newReport["sid"])
-        doc_ref.add({
-            u'sid': newReport["sid"],
-            u'category': newReport['category'],
-            u'contents': newReport['contents'],
-            u'time': newReport['time'],
-        })
+        doc_ref = getCollection(request.data["id"])
+        doc_ref.add(newReport)
         return Response({'data': newReport}, status=status.HTTP_201_CREATED)
 
-
-class ReportDetailView(APIView):
-    def get(self, request, report_pk, format=None):
-        report = Report.objects.filter(rid=report_pk).first()
-        if report:
-            serializer = ReportSerializer(report)
-            return Response({'data': serializer.data}, status=status.HTTP_200_OK)
-
-        else:
-            return Response({'data': 'NOT FOUND REPORT'}, status=status.HTTP_200_OK)
-
-    # def delete(self, request, doc_id, format=None):
-        # doc_ref = getCollection(newReport["sid"])
-        # # report = Report.objects.get(rid=report_pk)
-        # if report:
-        #     report.delete()
-        #     return Response({'data': 'SUCCESS'}, status=status.HTTP_200_OK)
-        # else:
-        #     return Response({'data': 'NOT FOUND REPORT'}, status=status.HTTP_200_OK)
+    def delete(self, request):
+        ## query에 id와 reportDocId 를 보내야 함
+        doc_ref = getCollection(request.GET.get("id"))
+        doc_ref.document(request.GET.get("reportDocId")).delete()
+        return Response({'data': 'SUCCESS'}, status=status.HTTP_200_OK)
 
 
-def getCollection(sid):
-    doc_ref = db.collection(u'reports').document(sid[:2])
-    doc_ref = doc_ref.collection(u'열차번호').document(sid[2:4])
-    doc_ref = doc_ref.collection(u'칸번호').document(sid[4:6])
-    doc_ref = doc_ref.collection(u'messages')
+def getCollection(id):
+    doc_ref = db.collection(u'reports').document(id[:2]).collection(u'messages')
     return doc_ref
