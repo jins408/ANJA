906d0c923d5a3bf3c01c2b6f2ffe9b1aaeee65b6
조민기 <hodduco_o@naver.com>
2020-11-09 14:56:27 +0900

    S03P31B101-160 /Update 관리자 로그인 수정

--- back-end/users/urls.py
+++ back-end/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.UserView.as_view(), name="users"),
+    path('', views.UserView.as_view(), name="users"),
     path('/<int:user_pk>', views.UserDetailView.as_view(), name="user_object"),
+    path('/sign', views.UserSessionView.as_view(), name="user_session"),
 ]
