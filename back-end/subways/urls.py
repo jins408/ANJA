68dfa767b3442a0465d7f1f06d0400137383d1d3
조민기 <hodduco_o@naver.com>
2020-11-10 15:52:20 +0900

    S03P31B101-148/Update 지하철 전체 시간표

--- back-end/subways/urls.py
+++ back-end/subways/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('/estimate', views.SubwayEstimatedTimeView.as_view(), name="subways_estimated_time"),
     path('/approach', views.SubwayApproachView.as_view(), name="subways_approach"),
     path('/timetable', views.SubwayTimeTableView.as_view(), name="subways_times"),
+    path('/station', views.SubwayStationView.as_view(), name="subways_stations")
     # path('/test', views.Test.as_view(), name="test"),
 ]
