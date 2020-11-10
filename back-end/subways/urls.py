de2ee0c62756fc60151d8e16e49c5a4ed04b2ce6
조민기 <hodduco_o@naver.com>
2020-11-10 15:28:19 +0900

    S03P31B101-148/Update 지하철 전체 시간표 및 환승 노선 구하기

--- back-end/subways/urls.py
+++ back-end/subways/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('/estimate', views.SubwayEstimatedTimeView.as_view(), name="subways_estimated_time"),
     path('/approach', views.SubwayApproachView.as_view(), name="subways_approach"),
     path('/timetable', views.SubwayTimeTableView.as_view(), name="subways_times"),
-    path('/test', views.Test.as_view(), name="test"),
+    # path('/test', views.Test.as_view(), name="test"),
 ]
